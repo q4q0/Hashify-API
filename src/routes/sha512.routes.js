81cabf22a48f35cf6ba96c68f350ede9b0ca469d
~/q4q0 <61029571+q4q0@users.noreply.github.com>
2022-01-20 22:55:14 +0300

    create sha512 routers & service & controller

---
+++ src/routes/sha512.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const sha2Controller = require("../controllers/sha1.controller");
+
+router.get("/", sha2Controller.getSH512Hashed);
+router.get("/save", sha2Controller.getAndSaveSHA512Hashed);
+router.get("/all", sha2Controller.getSavedSHA512Hashed);
+
+module.exports = router;
