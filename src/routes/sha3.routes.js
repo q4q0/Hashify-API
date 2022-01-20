67875044bace2103c2637a3e886ec85e0e1700d2
~/q4q0 <61029571+q4q0@users.noreply.github.com>
2022-01-20 22:54:30 +0300

    create sha2, sha2 routers & services

--- src/routes/sha3.routes.js
+++ src/routes/sha3.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const sha2Controller = require("../controllers/sha1.controller");
 
-router.get("/", sha2Controller.getSH2Hashed);
-router.get("/save", sha2Controller.getAndSaveSHA2Hashed);
-router.get("/all", sha2Controller.getSavedSHA2Hashed);
+router.get("/", sha2Controller.getSH3Hashed);
+router.get("/save", sha2Controller.getAndSaveSHA3Hashed);
+router.get("/all", sha2Controller.getSavedSHA3Hashed);
 
 module.exports = router;
--- src/services/sha2.service.js
+++ src/services/sha2.service.js
@@ -1,1 +1,40 @@
+const { sha2 } = require("hash-wasm");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
+const getSH2Hashed = async (req, res, next) => {
+  const { value } = req.query;
+  const hashedValue = await sha2(value);
+  return hashedValue;
+};
+
+const getSavedSH2Hashed = async (req, res, next) => {
+  const hashedValues = await prisma.hash.findMany({
+    where: {
+      type: "SHA2",
+    },
+  });
+  return hashedValues;
+};
+
+const saveSHA2Hashed = async (hashed) => {
+  try {
+    const savedHashed = await prisma.hash.create({
+      data: {
+        digest: hashed,
+        digestEncode: "hex",
+        type: "SHA2",
+        key: "",
+      },
+    });
+    return savedHashed;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+module.exports = {
+  getSH2Hashed,
+  getSavedSH2Hashed,
+  saveSHA2Hashed,
+};
