a7540b6518e7aadfd981545e3cb1c84e691d410f
~/q4q0 <61029571+q4q0@users.noreply.github.com>
2022-01-21 20:47:53 +0300

    update aes128 module

--- src/controllers/encryption/aes128.controller.js
+++ src/controllers/encryption/aes128.controller.js
@@ -1,29 +1,32 @@
 const aes128Service = require("../../services/encryption/aes128.service");
+
 const getAES128Encrypted = (req, res, next) => {
   try {
-    const { value, key } = req.query;
-    const result = aes128Service.getAES128Encrypted(value, key);
+    const { value, key, encoding } = req.query;
+    const result = aes128Service.getAES128Encrypted(value, key, encoding);
     res.status(200).json({
       success: true,
-      encrypted_value: result[0],
-      iv: result[1],
+      encrypted_value: result,
       type: "AES128",
     });
   } catch (err) {
+    console.log(err);
     throw new Error("something went wrong");
   }
 };
 
 const getAES128Decrypted = (req, res, next) => {
   try {
-    const { value, key, iv } = req.query;
-    const result = aes128Service.getAES128Encrypted(value, key, iv);
+    const { value, key, encoding } = req.query;
+    const result = aes128Service.getAES128Decrypted(value, key, encoding);
+    console.log(result);
     res.status(200).json({
       success: true,
       decrypted_value: result,
       type: "AES128",
     });
   } catch (err) {
+    console.log(err);
     throw new Error("something went wrong");
   }
 };
--- src/services/encryption/aes128.service.js
+++ src/services/encryption/aes128.service.js
@@ -1,39 +1,45 @@
 const crypto = require("crypto");
 
-const getAES128Encrypted = (value, key) => {
+const getAES128Encrypted = (value, key, encoding) => {
   try {
-    let updatedKey = crypto
+    const updatedKey = crypto
       .createHash("sha256")
       .update(String(key))
       .digest("base64")
       .slice(0, 16);
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-128-cbc", updatedKey, iv);
-    let result = cipher.update(value, "utf8", "base64");
-    result += cipher.final("base64");
-    return [result.toString("hex"), iv.toString("hex")];
+    const cipher = crypto.createCipheriv("aes-128-ecb", updatedKey, null);
+    const res = Buffer.concat([cipher.update(value), cipher.final()]).toString(
+      encoding
+    );
+    return res;
   } catch (err) {
     console.log(err);
+    throw new Error("Wow bro what are u talking about man?");
   }
 };
+
+const getAES128Decrypted = (value, key, encoding) => {
+  try {
+    const updatedValue = Buffer.from(value, "base64");
+    const updatedKey = crypto
+      .createHash("sha256")
+      .update(String(key))
+      .digest("base64")
+      .slice(0, 16);
+    const cipher = crypto.createDecipheriv("aes-128-ecb", updatedKey, null);
+    const res = Buffer.concat([
+      cipher.update(updatedValue),
+      cipher.final(),
+    ]).toString(encoding);
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Wow bro what are u talking about man?");
+  }
+};
+
 const saveAES128Encrypted = () => {};
 const saveAES128Decrypted = () => {};
-
-const getAES128Decrypted = (value, key, iv) => {
-  try {
-    let updatedKey = crypto
-      .createHash("sha256")
-      .update(String(key))
-      .digest("base64")
-      .slice(0, 16);
-    const decipher = crypto.createDecipheriv("aes-128-cbc", updatedKey, iv);
-    let result = decipher.update(value, "base64");
-    result += decipher.final();
-    return result.toString("hex");
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 module.exports = {
   getAES128Encrypted,
